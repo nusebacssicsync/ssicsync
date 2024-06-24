08796cea28100bb7227b743240934693fb41583a
yorwel <nusebac@gmail.com>
2024-06-24 16:01:27 +0800

    c

--- pages/6_Test.py
+++ pages/6_Test.py
@@ -6,6 +6,6 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("nusebacra_bucket1/SSIC Fact Ref Table.xlsx", input_format="xlsx", ttl=600)
+df = conn.read("nusebacra_bucket1/SSIC Fact Ref Table.csv", input_format="csv", ttl=600)
 
 st.table(df)
